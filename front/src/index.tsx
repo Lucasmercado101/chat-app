2dfff873f3bc0067d440c2116d82ee848a1fd2b5
Lucasmercado101 <lucasmercado101@gmail.com>
2021-07-30 13:14:01 -0300

    added react query devtools

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,12 +1,14 @@
 import React, { StrictMode } from 'react';
 import { render } from 'react-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 const queryClient = new QueryClient();
 render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <div>Hello world</div>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
   document.getElementById('root'),
