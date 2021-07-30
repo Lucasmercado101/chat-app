63a101334456a72daa23aee749bd3ea5ac603ac6
Lucasmercado101 <lucasmercado101@gmail.com>
2021-07-30 15:08:37 -0300

    added App.tsx and some styles reset

---
+++ front/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div
+      style={{ backgroundColor: '#1a1a1a', height: '100vh', width: '100vw' }}
+    >
+      a
+    </div>
+  );
+}
+
+export default App;
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,32 +1,33 @@
-import React, { StrictMode, useEffect } from 'react';
+import React, { StrictMode } from 'react';
 import { render } from 'react-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
-import { useGetPosts } from './api';
 import io from 'socket.io-client';
+import './styles.css';
+import App from './App';
 
 const socket = io('http://localhost:3000');
 
-function Test() {
-  useEffect(() => {
-    socket.emit('test', 'test', (resp: any) => {
-      console.log(resp);
-    });
-  }, [socket]);
+// function Test() {
+//   useEffect(() => {
+//     socket.emit('test', 'test', (resp: any) => {
+//       console.log(resp);
+//     });
+//   }, [socket]);
 
-  const a = useGetPosts();
-  return (
-    <div>
-      <pre>{JSON.stringify(a, null, 2)}</pre>
-    </div>
-  );
-}
+//   const a = useGetPosts();
+//   return (
+//     <div>
+//       <pre>{JSON.stringify(a, null, 2)}</pre>
+//     </div>
+//   );
+// }
 
 const queryClient = new QueryClient();
 render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Test />
+      <App />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
