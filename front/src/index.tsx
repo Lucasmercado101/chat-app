ab6b900684f41be30deb2bce6f3e5882eb47369a
Lucasmercado101 <lucasmercado101@gmail.com>
2021-07-30 13:11:17 -0300

    added react query

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,9 +1,13 @@
 import React, { StrictMode } from 'react';
 import { render } from 'react-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
+const queryClient = new QueryClient();
 render(
   <StrictMode>
-    <div>Hello world</div>
+    <QueryClientProvider client={queryClient}>
+      <div>Hello world</div>
+    </QueryClientProvider>
   </StrictMode>,
   document.getElementById('root'),
 );
