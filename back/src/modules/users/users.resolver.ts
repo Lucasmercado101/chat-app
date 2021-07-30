51c572ff705145bda813b6fb8065f192c19c0b1c
Lucasmercado101 <lucasmercado101@gmail.com>
2021-07-30 12:55:30 -0300

    added readonly to usersService

--- back/src/modules/users/users.resolver.ts
+++ back/src/modules/users/users.resolver.ts
@@ -4,7 +4,7 @@ import { UsersService } from './user.service';
 
 @Resolver(() => User)
 export class UsersResolver {
-  constructor(private usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) {}
 
   @Query(() => User)
   async user(@Args('id') id: string) {
