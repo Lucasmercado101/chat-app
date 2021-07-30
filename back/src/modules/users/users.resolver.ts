2b0815d633edeebabcbcfbf107caca973ec458d2
Lucasmercado101 <lucasmercado101@gmail.com>
2021-07-30 16:19:11 -0300

    removed unused import

--- back/src/modules/users/users.resolver.ts
+++ back/src/modules/users/users.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { User } from './user.model';
 import { UsersService } from './users.service';
 
