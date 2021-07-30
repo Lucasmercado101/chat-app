00a69a8979f85b8d4e979d299ccada185d2dfcc1
Lucasmercado101 <lucasmercado101@gmail.com>
2021-07-30 15:36:20 -0300

    added styles

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -5,7 +5,47 @@ function App() {
     <div
       style={{ backgroundColor: '#1a1a1a', height: '100vh', width: '100vw' }}
     >
-      a
+      <div
+        style={{
+          maxWidth: 1000,
+          margin: 'auto',
+          padding: '0 25px',
+          backgroundImage: `url()`,
+          height: '100%',
+          position: 'relative',
+          zIndex: 1,
+        }}
+      >
+        <img
+          style={{
+            position: 'absolute',
+            height: '100%',
+            width: '100%',
+            zIndex: -1,
+            opacity: 0.7,
+            left: 0,
+            right: 0,
+          }}
+          src="https://source.unsplash.com/RCAhiGJsUUE/1000x1000"
+        />
+        <div style={{ display: 'flex', height: '100%', padding: 50 }}>
+          <div
+            style={{
+              height: 40,
+              marginTop: 'auto',
+              display: 'flex',
+              width: '100%',
+              fontSize: 19,
+            }}
+          >
+            <input
+              style={{ color: 'black', width: '100%', fontSize: 19 }}
+              type="text"
+            />
+            <button style={{ color: 'black' }}>Send</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
