0b52a5cc581891779d25ace855b630b4e7297a36
Lucasmercado101 <lucasmercado101@gmail.com>
2021-07-30 12:27:24 -0300

    added mongoose

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
 import * as path from 'path';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot({
       autoSchemaFile: path.join(process.cwd(), 'src/schema.gql'),
     }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
     UsersModule,
   ],
 })
