dd24e31ad8a4a84a6fc01361fe10763877d9ccb1
Lucasmercado101 <lucasmercado101@gmail.com>
2021-07-30 13:08:58 -0300

    added react to front

---
+++ front/src/index.tsx
@@ -0,0 +1,9 @@
+import React, { StrictMode } from 'react';
+import { render } from 'react-dom';
+
+render(
+  <StrictMode>
+    <div>Hello world</div>
+  </StrictMode>,
+  document.getElementById('root'),
+);
