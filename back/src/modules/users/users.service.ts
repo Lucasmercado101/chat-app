7a745af300486b09a652c76556f2a45c4b5acd8c
Lucasmercado101 <lucasmercado101@gmail.com>
2021-08-06 17:18:52 -0300

    create creates a mongo user

--- back/src/modules/users/users.service.ts
+++ back/src/modules/users/users.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './user.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return this.userModel.create(createUserDto);
   }
 
   findAll() {
