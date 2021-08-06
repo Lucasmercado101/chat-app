7595e8ef1808e5d6492e3d373d6c266b7bfea889
Lucasmercado101 <lucasmercado101@gmail.com>
2021-08-06 18:35:27 -0300

    created

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './modules/users/users.module';
 import { MessagesModule } from './modules/messages/messages.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
@@ -10,6 +12,8 @@ import { MongooseModule } from '@nestjs/mongoose';
     UsersModule,
     MessagesModule,
     MongooseModule.forRoot(process.env.MONGO_URI),
+    AuthModule,
   ],
+  controllers: [AppController],
 })
 export class AppModule {}
