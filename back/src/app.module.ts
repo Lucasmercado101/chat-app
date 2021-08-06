12543cb63799d238c7612f717751851df0f2cfdd
Lucasmercado101 <lucasmercado101@gmail.com>
2021-08-06 16:25:31 -0300

    added mongo

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { MessagesModule } from './modules/messages/messages.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule, MessagesModule],
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    MessagesModule,
+    MongooseModule.forRoot(process.env.MONGO_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
