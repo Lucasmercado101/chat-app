62327a9ef197de4fb9d138d7ab3f70dd281d988e
Lucasmercado101 <lucasmercado101@gmail.com>
2021-08-06 15:14:08 -0300

    created back again

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,20 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { GraphQLModule } from '@nestjs/graphql';
-import { MongooseModule } from '@nestjs/mongoose';
-import * as path from 'path';
-import { EventsModule } from './modules/events/events.module';
-import { UsersModule } from './modules/users/users.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    GraphQLModule.forRoot({
-      autoSchemaFile: path.join(process.cwd(), 'src/schema.gql'),
-    }),
-    MongooseModule.forRoot(process.env.MONGO_URI),
-    UsersModule,
-    EventsModule,
-  ],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
