30e18edd9ff62c735420162c168d24dd4f102908
Lucasmercado101 <lucasmercado101@gmail.com>
2021-08-06 17:01:42 -0300

    removed appservice

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
-import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { MessagesModule } from './modules/messages/messages.module';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -14,6 +13,5 @@ import { MongooseModule } from '@nestjs/mongoose';
     MongooseModule.forRoot(process.env.MONGO_URI),
   ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
