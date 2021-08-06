202c75bfac76b62b4c203c1bae837280ed7e2180
Lucasmercado101 <lucasmercado101@gmail.com>
2021-08-06 17:49:34 -0300

    commented out update and remove

--- back/src/modules/users/users.controller.ts
+++ back/src/modules/users/users.controller.ts
@@ -38,13 +38,13 @@ export class UsersController {
     return responseUserDto(await this.usersService.findOne(id));
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  //   return this.usersService.update(+id, updateUserDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.usersService.remove(+id);
+  // }
 }
--- back/src/modules/users/users.service.ts
+++ back/src/modules/users/users.service.ts
@@ -21,11 +21,11 @@ export class UsersService {
     return this.userModel.findById(id).exec();
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+  // update(id: number, updateUserDto: UpdateUserDto) {
+  //   return `This action updates a #${id} user`;
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+  // remove(id: number) {
+  //   return `This action removes a #${id} user`;
+  // }
 }
