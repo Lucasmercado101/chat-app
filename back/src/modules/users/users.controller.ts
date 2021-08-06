d87afbcb64cbe718a71bedae3309832af7f387cd
Lucasmercado101 <lucasmercado101@gmail.com>
2021-08-06 17:45:44 -0300

    created function to transform user to responseUserDto

--- back/src/modules/users/users.controller.ts
+++ back/src/modules/users/users.controller.ts
@@ -10,13 +10,27 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserDocument } from './user.schema';
+
+function responseUserDto(user: UserDocument) {
+  return {
+    id: user.id,
+    userName: user.userName,
+  };
+}
+
+interface ResponseUserDto {
+  id: string;
+  userName: string;
+}
+
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  async create(@Body() createUserDto: CreateUserDto) {
-    return await this.usersService.create(createUserDto);
+  async create(@Body() createUserDto: CreateUserDto): Promise<ResponseUserDto> {
+    return responseUserDto(await this.usersService.create(createUserDto));
   }
 
   @Get()
